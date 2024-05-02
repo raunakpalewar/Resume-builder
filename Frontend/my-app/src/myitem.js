26ea290e3ca4fe16fa3cabc543d4629a27d9275e
Raunak Palewar <raunakpalewar9979@gmail.com>
2024-05-02 16:17:03 +0530

    test commit

--- Frontend/my-app/src/myitem.js
+++ Frontend/my-app/src/myitem.js
@@ -5,14 +5,25 @@ import React from 'react';
 
 class Item extends React.Component{
     clickme(){
+        this.setState({
+            clicks:this.state.clicks+1
+        })
         alert("i was clicked")
     }
-    
+    constructor(props){
+        super(props)
+        this.state={
+            clicks:0
+        }
+    }
     render(){
       return(
         <div>
             <h1 onClick={()=>this.clickme()}>Welcome to React Learning with {this.props.name} & {this.props.Name}</h1>
             <h2 >Hello from Raunak Palewar</h2>
+            <span>
+                {this.state.clicks} number of clicks
+            </span>
         </div>
       )
     }
