67aeb911d4e0f39fbc8c4a9bd53478c97b9b8f54
Raunak Palewar <raunakpalewar9979@gmail.com>
2024-11-06 18:01:55 +0530

    a

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -19,6 +19,7 @@ from django.core.mail import send_mail
 from datetime import timedelta
 from django.db.models import Q
 import re
+
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
